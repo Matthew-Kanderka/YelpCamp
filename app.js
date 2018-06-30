21e2e1cefe8313250a613da9f4b639c37ef2c71a
Matthew-Kanderka <kanderkam@hotmail.com>
2018-06-29 23:23:45 -0600

    added more pages

--- app.js
+++ app.js
@@ -1,36 +1,94 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var Campground = require("./models/campground");
+var seedDB = require("./seeds");
+var Comment = require("./models/comment");
 
-var bodyParser = require("body-parser")
-
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"))
+seedDB();
 
-var campgrounds = [
-	{name: "Camp Gardener", image: "http://img.groundspeak.com/waymarking/display/96ce9df2-0423-42c1-ac03-f40ebb6873fa.jpg"},
-	{name: "Camp Impeesa", image: "http://campimpeesa.ca/assets/image-cache/uploads/images/DSC_7654.a3555272.jpg"},
-	{name: "Camp Woods", image: "http://www.bradwoods.org/wp-content/uploads/2012/02/camping.jpg"}
-];
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds :campgrounds});
+
+	Campground.find({}, function(err, allCampgrounds){
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds/index", {campgrounds:allCampgrounds});
+		}
+	});
+	//res.render("campgrounds", {campgrounds :campgrounds});
 });
 
 app.post("/campgrounds", function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image}
-	campgrounds.push(newCampground);
+	var description = req.body.description;
+	var newCampground = {name: name, image: image, description: description}
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+	//campgrounds.push(newCampground);
 
-	res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req, res){
-	res.render("new");
+	res.render("campgrounds/new");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds/show", {campground: foundCampground})
+		}
+	});
+});
+
+//---------------------- comments
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: campground});
+		}
+	})
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			Comment.create(req.body.comment, function(err, comment){
+				if(err) {
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			})
+		}
+	})
 });
 
 app.listen(3000, function(){
