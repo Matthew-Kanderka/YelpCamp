894dbc1853af6833d95e74bbba093942f21d3c7f
Matthew-Kanderka <kanderkam@hotmail.com>
2018-07-23 21:23:22 -0600

    final push

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-var express = require('express'),
-	app = express(),
-	mongoose = require('mongoose'),
-	passport = require("passport"),
-	LocalStrategy = require("passport-local"),
-	bodyParser = require("body-parser"),
-	Campground = require("./models/campground"),
-	methodOverride = require("method-override")
-	seedDB = require("./seeds"),
-	Comment = require("./models/comment"),
-	User = require("./models/user");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds");
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
 
-var commentRoutes = require("./routes/comments"),
-	campgroundRoutes = require("./routes/campgrounds"),
-	authRoutes = require("./routes/index");
+ //require moment
+// app.locals.moment = require('moment');
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// connect to  database
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+mongoose.connect(url);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-// seedDB();
+app.use(flash());
+// seedDB(); //seed the database
 
-// passport config
+// PASSPORT CONFIGURATION
 app.use(require("express-session")({
-	secret: "dog",
-	resave: false,
-	saveUninitialized: false
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -35,13 +43,22 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
-	res.locals.currentUser = req.user;
-	next();
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
 });
 
-app.use("/", authRoutes);
+//pass in path to view
+app.use(function(req, res, next) {
+  res.locals.current_path = req.path;
+  next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-app.use("/campgrounds",campgroundRoutes);
+
 
 app.listen(3000, function(){
 	console.log("Server started");
